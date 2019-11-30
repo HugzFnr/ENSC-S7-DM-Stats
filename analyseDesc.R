937ce5a00ee4664b3f2ee98fc2837642e94460c9
Your Name <lbregier@ensc.fr>
2019-11-30 10:24:44 +0100

    Analyse descriptive :Coefficients de corrélation, BoxPlots et tests de student

--- analyseDesc.R
+++ analyseDesc.R
@@ -1,18 +1,27 @@
 donnees<-readRDS("activation2020.rdata")
+
+###### Visualisation de la corrélation des données #########
 donnees_cop<-donnees[,-c(1,2,3)]
 donnees_cop1<-donnees[,-c(2,3)]
 plot(donnees_cop)
 
-#install.packages("corrplot")
+install.packages("corrplot")
 library("corrplot")
 corrplot(cor(donnees_cop))
 
-#tentative d'ACP
-#install.packages("PCAmixdata")
+#Il semberait que Prod_G_Frontal_Inf_Tri_1_L soit corrélés avecProd_G_Frontal_Inf_Tri_1_R et Prod_S_Sup_Temporal_4_L
+cor(donnees$Prod_G_Frontal_Inf_Tri_1_L, donnees$Prod_G_Frontal_Inf_Tri_1_R)
+cor(donnees$Prod_G_Frontal_Inf_Tri_1_L, donnees$Prod_S_Sup_Temporal_4_L)
+
+
+###### ACP ################
+
+install.packages("PCAmixdata")
 library(PCAmixdata)
-resACP <- PCAmix(donnees_cop1,graph=FALSE)
+donnees_cop1Numeric <-as.numeric(donnees_cop1)
+resACP <- PCAmix(donnees_cop1Numeric,graph=FALSE)
 round(resACP$eig,digit=2)
-#ne conserver que les dim oÃ¹ "eignvalue">1
+#ne conserver que les dimension "eignvalue">1
 #donc dim 1,2,3,4,5, et 6?
 #recu^peration de 68.59% de l'info ou 75.24% si on garde dim 6
 
@@ -25,3 +34,30 @@ plot(resACP,choice="ind") #j'arrive pas Ã  nommer les points
 res<-PCAmix(X.quanti = donnees_cop1[,1:16], graph=TRUE)
 round(res$eig, digit=2)
 plot(res,choice="ind")
+
+
+
+####### Variable qualitatives Boxplots ##########
+varExplique <- donnees$Prod_G_Frontal_Inf_Tri_1_L
+
+par(mfrow=c(1,2)) 
+boxplot(varExplique~donnees$Preference_Manuelle, xlab="Préférence Manuelle", ylab="Aire de Broca", col=c("aliceblue","blanchedalmond"))
+boxplot(varExplique~donnees$Sexe, xlab="Sexe", ylab="Aire de Broca", col=c("aliceblue","blanchedalmond"))
+testPM <- boxplot(varExplique~donnees$Preference_Manuelle, xlab="Préférence Manuelle", ylab="Aire de Broca", col=c("aliceblue","blanchedalmond"), plot=F)$stats
+testS <- boxplot(varExplique~donnees$Sexe, xlab="Sexe", ylab="Aire de Broca",col=c("aliceblue","blanchedalmond"), plot=F)$stats
+
+#Test de Student
+PM_droite<-donnees[donnees$Preference_Manuelle=="D",colnames(donnees)!="Preference_Manuelle"] 
+PM_gauche<-donnees[donnees$Preference_Manuelle=="G",colnames(donnees)!="Preference_Manuelle"] 
+t.test(PM_droite$Prod_G_Frontal_Inf_Tri_1_R, PM_gauche$Prod_G_Frontal_Inf_Tri_1_R)
+
+
+Sexe_F<-donnees[donnees$Sexe=="F",colnames(donnees)!="Sexe"] 
+Sexe_H<-donnees[donnees$Sexe=="H",colnames(donnees)!="Sexe"] 
+t.test(Sexe_F$Prod_G_Frontal_Inf_Tri_1_R, Sexe_H$Prod_G_Frontal_Inf_Tri_1_R)
+
+
+
+
+
+#rm(list=ls())
